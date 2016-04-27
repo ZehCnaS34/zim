001674a0b90897f575b9aa709dd832835c1be4df
Alexander Sanchez <asanchez.dev@gmail.com>
2016-04-27 15:17:16 -0400

    works kinda

--- install.jl
+++ install.jl
@@ -2,14 +2,27 @@
 
 dotvim() = homedir() |> (hd) -> joinpath(hd, ".vim") 
 
-hasdotvim = dotvim() |> isdir 
+hasdotvim = string(dotvim()) |> isdir 
 
 if hasdotvim
-  #=// copy file=#
   println("Add extension to $dotvim()")
-  exts = read(STDIN, Char)
-  outputlocation = join(dotvim(), exts, ".") 
-  println(outputlocation)
-  #=cp(dotvim(), )=#
+  outputlocation = dotvim() * ".backup"
+  println("Backing up Old Vim to $(dotvim() * ".backup")")
+  cp(dotvim(), outputlocation)
+  rm(dotvim(); recursive = true)
 end
 
+# must make better
+pull_command = `git clone https://github.com/ZehCnaS34/zim.git $(joinpath(homedir(), ".vim"))`
+run(pull_command)
+
+
+vundle_command = `git clone https://github.com/VundleVim/Vundle.vim.git $(joinpath(homedir(), ".vim/bundle/Vundle.vim"))`
+run(vundle_command)
+
+
+link_command = `ln -s $(joinpath(homedir(), ".vim/init.vim")) $(joinpath(homedir(), ".vimrc"))`
+run(link_command)
+
+vim_command = `vim -c "BundleInstall"`
+run(vim_command)
